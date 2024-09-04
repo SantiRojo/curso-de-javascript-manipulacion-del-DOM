dc58f6e2bbd591c32741ec6ed05368cc9897205e
Santi Rojo <santi.rojo.franco@gmail.com>
2024-09-04 00:09:59 -0300

    Hasta la clase 'El objeto evento o eventObject'

---
+++ Eventos/app.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const btn = document.querySelector('#btn');
+
+container.addEventListener('mouseover', () => {
+  container.style.backgroundColor = 'blue';
+});
+
+container.addEventListener('mouseout', () => {
+  container.style.backgroundColor = 'red';
+});
+
+/* btn.addEventListener('click', () => {
+  alert('Button clicked!');
+}) */
+
+const buttonClickCallback = () => {
+  alert('Button clicked!');
+}
+
+btn.addEventListener('click', buttonClickCallback);
+
+setTimeout(() => {
+  btn.removeEventListener('click', buttonClickCallback);
+}, 2000);
