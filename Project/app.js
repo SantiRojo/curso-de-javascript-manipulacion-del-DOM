337011b76808dded87b19a50ccd5a432f6cb2dab
Santi Rojo <santi.rojo.franco@gmail.com>
2024-09-07 02:26:11 -0300

    Finalización del curso

--- Project/app.js
+++ Project/app.js
@@ -2,6 +2,8 @@ const taskForm = document.getElementById('task-form');
 
 const taskList = document.getElementById('task-list');
 
+loadTasks();
+
 taskForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -11,20 +13,81 @@ taskForm.addEventListener('submit', (e) => {
   console.log(task);
   if (task) {
     taskList.append(createTaskElement(task));
+    storeTaskInLocalStorage(task);
     taskInput.value = '';
   }
 });
 
-const createTaskElement = (task) => {
+function createTaskElement (task) {
   const li = document.createElement('li');
   li.textContent = task;
-  li.append(createButton('❌', 'delete-btn'), createButton('✏️', 'edit'));
+  li.append(createButton('❌', 'delete-btn'), createButton('✏️', 'edit-btn'));
   return li;
 };
 
-const createButton = (text, className) => {
+function createButton(text, className) {
   const btn = document.createElement('span');
   btn.textContent = text;
   btn.className = className;
   return btn;
 };
+
+taskList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete-btn')) {
+    deleteTask(event.target.parentElement);
+  } else if (event.target.classList.contains('edit-btn')) {
+    editTask(event.target.parentElement);
+  }
+});
+
+function deleteTask (taskItem) {
+if (confirm('Estas seguro de borrar este elemento?')) {
+      taskItem.remove();
+  }
+}
+
+function editTask(taskItem) {
+  const newTask = prompt('Edita la tarea:', taskItem.firstChild.textContent);
+  if (newTask !== null) {
+    taskItem.firstChild.textContent = newTask;
+    updateLocalStorage();
+  }
+};
+
+function storeTaskInLocalStorage(task) {
+  const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  tasks.forEach((task) => {
+    taskList.appendChild(createTaskElement(task));
+  });
+};
+
+function updateLocalStorage() {
+  const tasks = Array.from(taskList.querySelectorAll('li')).map(li => 
+    li.firstChild.textContent
+  );
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+const themeToggleBtn = document.getElementById('toggle-theme-btn');
+
+const currentTheme = localStorage.getItem('theme');
+
+themeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme');
+
+  const theme = document.body.classList.contains('dark-theme') 
+  ? 'dark' 
+  : 'light';
+  localStorage.setItem('theme', theme);
+});
+
+if(currentTheme === 'dark') {
+  document.body.classList.add('dark-theme')
+}
